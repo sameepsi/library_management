66b96ce90cbfc2fbe4b3a01d61d30f91392e8058
Sameep Singhania <sameepsi@gmail.com>
2017-10-28 23:08:26 +0530

    changes in config file

--- controllers/user.js
+++ controllers/user.js
@@ -26,14 +26,21 @@ const create = (req, res, next) => {
   });
   user.save()
   .then((savedUser)=>{
-    return User.populate(savedUser, {path:"library"});
+    return User.populate(savedUser, {path:"library"})
+    .then(populatedUser=>{
+      console.log(populatedUser);
+      var token = authenticationUtil.generateAuthToken(populatedUser);
+      res.header('x-access-token', token).status(200).send(populatedUser);
+    })
+    .catch((err)=>{
+      console.log(err)
+      res.status(500).send({
+        message:err.message
+      });
+    });
 
   })
-  .then(populatedUser=>{
-    console.log(populatedUser);
-    var token = authenticationUtil.generateAuthToken(populatedUser);
-    res.header('x-access-token', token).status(200).send(populatedUser);
-  })
+
   .catch((err)=>{
     console.log(err)
     res.status(500).send({
@@ -120,7 +127,7 @@ const fetchUserIssuedBooks = (req, res, next) => {
   })
   .catch((err)=>{
     res.status(500).send({
-        message: err.message
+      message: err.message
     });
   });
 };
