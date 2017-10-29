5ce2e550bf33a9d73f04000a798dc35c1270cf8a
nitin sharma <nitin@ninja.local>
2017-10-29 21:14:24 +0530

    cors support

--- app.js
+++ app.js
@@ -24,6 +24,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+/***********************************************************************************************************************
+ * CORS
+ ***********************************************************************************************************************/
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-access-id, client-time");
+    res.header("Access-Control-Allow-Methods", 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 //register api routes
 app.use('/users', users);
 app.use('/library', library);
